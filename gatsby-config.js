c92d7628339e07b129a9900de63e39443c11e0a9
cdtinney <colintinney@gmail.com>
2019-01-29 14:34:54 -0500

    move Jekyll files to Gatsby locations

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Colin Tinney',
+    url: 'https://colintinney.com',
+    contact: {
+      name: 'Colin Tinney',
+      email: 'colintinney@gmail.com',
+      displayedEmail: 'colintinney [at] gmail.com',
+    },
+    social: {
+      gitHub: 'cdtinney',
+      linkedIn: 'cdtinney',
+      spotify: 'cdtinney',
+      goodreads: 'cdtinney',
+    },
+  },
+};
