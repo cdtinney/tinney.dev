39c700b6f27d04e8124affaf8472ef498c95e42a
cdtinney <colintinney@gmail.com>
2019-01-30 20:02:57 -0500

    add font link to head

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"></link>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
