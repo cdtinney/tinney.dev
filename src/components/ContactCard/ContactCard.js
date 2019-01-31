6d227b92163d7b4961ea87fb3e416f66b28eeb33
cdtinney <colintinney@gmail.com>
2019-01-31 10:21:48 -0500

    add social links
     Please enter the commit message for your changes. Lines starting

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 import { StaticQuery, graphql } from 'gatsby';
 
 import contactCardStyles from './ContactCard.module.css';
 
-function Icon({ title, href, label, icon }) {
-  return (
-    <a
-      class="contact-card__icon-container__icon"
-      title={title}
-      href={href}
-      target="_blank"
-      aria-label={label}
-    >
-      {icon}
-    </a>
-  );
-}
-
 function ContactCard({ data }) {
   return (
     <div className={contactCardStyles.container}>
-      <div className={contactCardStyles.container__icons}>
+      <div className={contactCardStyles.container__social}>
+        <a
+          title={`${data.site.siteMetadata.social.github} on GitHub`}
+          href={`https://github.com/${data.site.siteMetadata.social.github}`}
+          aria-label="GitHub profile"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className={contactCardStyles.icon}
+            size="lg"
+          />
+        </a>
+        <a
+          title={`${data.site.siteMetadata.social.linkedin} on LinkedIn`}
+          href={`https://linkedin.com/in/${data.site.siteMetadata.social.linkedin}`}
+          aria-label="LinkedIn profile"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className={contactCardStyles.icon}
+            size="lg"
+          />
+        </a>
       </div>
       <div className={contactCardStyles.container__email}>
         <FontAwesomeIcon
@@ -62,10 +71,8 @@ export default function ContactCardWithData() {
                   resumeUrl,
                 },
                 social {
-                  gitHub,
-                  linkedIn,
-                  spotify,
-                  goodreads
+                  github,
+                  linkedin,
                 }
               }
             }
