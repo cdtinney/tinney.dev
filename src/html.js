35a3c3c70199271fc2cf32add013146370039888
cdtinney <colintinney@gmail.com>
2019-02-01 20:22:02 -0500

    add title

--- src/html.js
+++ src/html.js
@@ -5,6 +5,7 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+        <title>colin tinney</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
