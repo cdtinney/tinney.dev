b785d6201e687067f013c175271873086ab9ef31
cdtinney <colintinney@gmail.com>
2019-02-01 20:31:14 -0500

    load font awesome CSS explicitly

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 import "./src/styles/reset.css"
 import "./src/styles/global.css"
+import "./node_modules/@fortawesome/fontawesome-svg-core/styles.css"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,10 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+
 import Home from './Home';
+
+// CSS for Font Awesome is explicitly loaded to ensure
+// it gets loaded before SVG elements are rendered
+config.autoAddCss = false
 
 // TODO How to change this?
 export default Home;
