73360f78300b5ab0aff88f8f4469ca322f1c43b5
cdtinney <colintinney@gmail.com>
2019-02-14 16:37:57 +0800

    cleanup html.js formatting

--- src/html.js
+++ src/html.js
@@ -1,5 +1,5 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function HTML(props) {
   return (
@@ -40,4 +40,4 @@ HTML.propTypes = {
   preBodyComponents: PropTypes.array,
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
-}
+};
