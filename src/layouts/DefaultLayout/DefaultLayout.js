8238e5152d2cea6cc2cd6dba4f016ad2327718b3
cdtinney <colintinney@gmail.com>
2019-02-28 13:36:24 +0800

    refactor: remove useless footer

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Footer from '../../components/Footer';
-
 import defaultLayoutStyles from './DefaultLayout.module.css';
 
 export default function DefaultLayout({ children }) {
   return (
-    <React.Fragment>
-      <main className={defaultLayoutStyles.content} aria-label="Content">
-        { children }
-      </main>
-      <Footer />
-    </React.Fragment>
+    <main className={defaultLayoutStyles.content} aria-label="Content">
+      { children }
+    </main>
   );
 }
 
